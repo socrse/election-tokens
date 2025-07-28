eac5dfd37f3f7ab52516c456752e9acf695cabc8
Mike Simpson <mike.simpson@ncl.ac.uk>
2025-07-28 23:04:28 +0100

    Specify encoding on open
    Avoids errors (on Windows)

--- src/election_tokens/__main__.py
+++ src/election_tokens/__main__.py
@@ -104,7 +104,8 @@ def generate(email_file: pathlib.Path,
                             config('SENDER_ADDRESS'), config('PASSWORD'),
                             config('SENDER_NAME'),
                             pathlib.Path('templates'))
-    with sender, open(email_file, 'r') as f_in, open(token_file, 'a') as f_out:
+
+    with sender, open(email_file, 'r', encoding='utf-16') as f_in, open(token_file, 'a', encoding='utf-16') as f_out:
         # Shuffle list so output tokens list can't be linked
         rows = list(csv.DictReader(f_in))
         random.shuffle(rows)
